a571846687c0a8681ebfb40fc714eb320318326f
PC@DESKTOP-S0OKBH5 <nuruladag@gmail.com>
2022-06-21 23:19:06 +0300

    ООП

---
+++ src/main/java/lessOOP/Cat.java
@@ -0,0 +1,13 @@
+package lessOOP;
+
+public class Cat {
+    String name;
+    String color;
+    int age;
+
+public Cat (String name, String color, int age) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+}
+}
---
+++ src/main/java/lessOOP/Main.java
@@ -0,0 +1,13 @@
+package lessOOP;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat("Барсик", "красный", 2);
+        Cat cat2 = new Cat("Мурзик", "белый", 1);
+
+        System.out.println(cat2.color);
+        System.out.println(cat1.color);
+
+    }
+}
